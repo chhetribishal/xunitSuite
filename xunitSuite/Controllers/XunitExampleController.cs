0a4e5f184dd5727314d8c7e5317fbad4e8076554
chhetribishal <bishal.in.thuma012@gmail.com>
2025-03-01 22:27:52 +0545

    Added Unit test Project

---
+++ xunitSuite/Controllers/XunitExampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xunitSuite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class XunitExampleController : ControllerBase
+    {
+        [HttpGet("index/{guessedNumber}")]
+        public string Index(int guessedNumber)
+        {
+            if(guessedNumber < 100)
+            {
+                return "Wrong! Try a bigger number.";
+            }
+            else if (guessedNumber >100)
+            {
+                return "Wrong! Try a smaller number.";
+            }
+            else
+            {
+                return "You guessed correct number";
+            }
+        }
+    }
+}
---
+++ xunitSuiteTests/Controllers/XunitExampleControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xunitSuite.Controllers;
+
+namespace xunitSuiteTests.Controllers
+{
+    public class XunitExampleControllerTest
+    {
+
+        [Fact] // If there are no parameter to send while testing
+
+        //controllername_controllermethodname_Testingfunctionality
+        public void XunitExampleController_Index_ValidLargeNumberResult()
+        {
+            ///AAA
+            ///Arrange -
+
+            XunitExampleController controller = new XunitExampleController();
+            int guessedNumber = 120;
+            string expectedResult = "Wrong! Try a smaller number.";
+            //Act -
+
+            string result = controller.Index(guessedNumber);
+            //Assert -
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact] // If there are no parameter to send while testing
+
+        //controllername_controllermethodname_Testingfunctionality
+        public void XunitExampleController_Index_ValidSmallerNumberResult()
+        {
+            ///AAA
+            ///Arrange -
+
+            XunitExampleController controller = new XunitExampleController();
+            int guessedNumber = 80;
+            string expectedResult = "Wrong! Try a bigger number.";
+            //Act -
+
+            string result = controller.Index(guessedNumber);
+            //Assert -
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact] // If there are no parameter to send while testing
+
+        //controllername_controllermethodname_Testingfunctionality
+        public void XunitExampleController_Index_ValidCorrectNumberResult()
+        {
+            ///AAA
+            ///Arrange -
+
+            XunitExampleController controller = new XunitExampleController();
+            int guessedNumber = 100;
+            string expectedResult = "You guessed correct number";
+            //Act -
+
+            string result = controller.Index(guessedNumber);
+            //Assert -
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
